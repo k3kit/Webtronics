a75ce2ba392290a13bda95dcf1a111bcddf9f265
k3kit <hadouken7809@gmail.com>
2023-02-06 22:21:31 +0400

    feat: add sass configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+import localFont from "@next/font/local";
+import "../styles/main.scss";
 
+const inter = Inter({
+  weight: ["600", "400"],
+  style: "normal",
+  variable: "--inter-font",
+});
+const ClashDisplay = localFont({
+  src: [
+    {
+      path: "./ClashDisplay-Semibold.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./ClashDisplay-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--clash-display",
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --clash-display: ${ClashDisplay.style.fontFamily};
+          --inter-font: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
